57f5d586e87203ca0fa15ef5b3dadb1d47abd194
vishal Raj <rvishl05@gmail.com>
2022-12-31 12:45:35 +0530

    change

--- src/responsive.js
+++ src/responsive.js
@@ -3,7 +3,7 @@ import { css } from "styled-components";
 
 const phoneView = (props) => {
     return css`
-        @media only screen and (max-width: 380px) {
+        @media only screen and (max-width: 395px) {
             ${props}
         }
     
